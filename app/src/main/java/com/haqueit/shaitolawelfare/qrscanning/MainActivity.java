e29199da590d6c421855384ef8f02777f0732ed0
Enamul <ehaque95@gmail.com>
2020-02-04 11:45:11 +0600

    first commit

---
+++ app/src/main/java/com/haqueit/shaitolawelfare/qrscanning/MainActivity.java
@@ -0,0 +1,59 @@
+package com.haqueit.shaitolawelfare.qrscanning;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
+
+public class MainActivity extends AppCompatActivity {
+
+    int PERMISSION_ALL = 1;
+    String[] PERMISSIONS = {
+            Manifest.permission.CAMERA,
+            android.Manifest.permission.INTERNET,
+
+
+    };
+
+    Button code_scanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(!hasPermissions(this, PERMISSIONS)){
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+
+        code_scanner = findViewById(R.id.code_scanner);
+        code_scanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,Code_Scanner.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
